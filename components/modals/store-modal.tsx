e1ea7c903657e330fb2afa4e90e408cb39f29667
anthonysuarez <antsuarez305@gmail.com>
2023-09-02 08:30:31 -0500

    Finish form components

--- components/modals/store-modal.tsx
+++ components/modals/store-modal.tsx
@@ -12,8 +12,10 @@ import {
   FormField,
   FormItem,
   FormLabel,
+  FormMessage,
 } from '@/components/ui/form'
 import { Input } from '@/components/ui/input'
+import { Button } from '@/components/ui/button'
 
 const formSchema = z.object({
   name: z.string().min(1),
@@ -54,10 +56,16 @@ export const StoreModal = () => {
                     <FormControl>
                       <Input placeholder='E=Commerce' {...field} />
                     </FormControl>
+                    <FormMessage />
                   </FormItem>
                 )}
               />
-              <div className='pt-6 space-x-2 flex items-center justify-end'></div>
+              <div className='pt-6 space-x-2 flex items-center justify-end w-full'>
+                <Button variant='outline' onClick={storeModal.onClose}>
+                  Cancel
+                </Button>
+                <Button type='submit'>Continue</Button>
+              </div>
             </form>
           </Form>
         </div>
