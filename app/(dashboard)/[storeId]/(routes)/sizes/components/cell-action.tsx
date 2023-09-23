1a9e0c8fad137c7dcf7123fce751e1802458ba2e
anthonysuarez <antsuarez305@gmail.com>
2023-09-22 21:40:22 -0500

    Create API routes for Sizes page

--- app/(dashboard)/[storeId]/(routes)/sizes/components/cell-action.tsx
+++ app/(dashboard)/[storeId]/(routes)/sizes/components/cell-action.tsx
@@ -15,11 +15,11 @@ import {
 } from '@/components/ui/dropdown-menu'
 import { Button } from '@/components/ui/button'
 
-import { BillboardColumn } from './columns'
+import { SizeColumn } from './columns'
 import { AlertModal } from '@/components/modals/alert-modal'
 
 interface CellActionProps {
-  data: BillboardColumn
+  data: SizeColumn
 }
 
 export const CellAction: React.FC<CellActionProps> = ({ data }) => {
@@ -31,21 +31,17 @@ export const CellAction: React.FC<CellActionProps> = ({ data }) => {
 
   const onCopy = (id: string) => {
     navigator.clipboard.writeText(id)
-    toast.success('Billboard Id copied to the clipboard')
+    toast.success('Size Id copied to the clipboard')
   }
 
   const onDelete = async () => {
     try {
       setLoading(true)
-      await axios.delete(
-        `/api/${params.storeId}/billboards/${data.id}`
-      )
+      await axios.delete(`/api/${params.storeId}/sizes/${data.id}`)
       router.refresh()
-      toast.success('Billboard Deleted.')
+      toast.success('Size Deleted.')
     } catch (error) {
-      toast.error(
-        'Make sure you removed all categories using this billboard first.'
-      )
+      toast.error('Make sure you removed all products using this size first.')
     } finally {
       setLoading(false)
       setOpen(false)
@@ -75,7 +71,7 @@ export const CellAction: React.FC<CellActionProps> = ({ data }) => {
           </DropdownMenuItem>
           <DropdownMenuItem
             onClick={() =>
-              router.push(`/${params.storeId}/billboards/${data.id}`)
+              router.push(`/${params.storeId}/sizes/${data.id}`)
             }
           >
             <Edit className='mr-2 h-4 w-4' />
